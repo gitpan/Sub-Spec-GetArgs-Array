03c61aa33fd8b8daea13cc4bcf925ed21b08684f
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-20 08:55:14 +0000

    Import of SHARYANTO/Sub-Spec-GetArgs-Array-0.06 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-GetArgs-Array
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-GetArgs-Array-0.06.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sub/Spec/GetArgs/Array.pm
+++ lib/Sub/Spec/GetArgs/Array.pm
@@ -11,7 +11,7 @@ use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(get_args_from_array);
 
-our $VERSION = '0.05'; # VERSION
+our $VERSION = '0.06'; # VERSION
 
 our %SPEC;
 
@@ -122,7 +122,7 @@ Sub::Spec::GetArgs::Array - Get subroutine arguments from array
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 SYNOPSIS
 
@@ -131,6 +131,12 @@ version 0.05
  my $res = get_args_from_array(array=>\@ary, spec=>$spec, ...);
 
 =head1 DESCRIPTION
+
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
 
 This module provides get_args_from_array() (and gencode_get_args_from_array(),
 upcoming). This module is used by, among others, L<Sub::Spec::GetArgs::Argv> and
@@ -199,7 +205,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
